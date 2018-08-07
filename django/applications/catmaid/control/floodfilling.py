6278e6bc07243816bd79eb9022f94644a68ac251
willp24 <willp24@vt.edu>
2018-08-07 12:04:30 -0400

    Floodfilling:
    Added an endpoint for floodfilling along with a control file to handle
    calls to it.

---
+++ django/applications/catmaid/control/floodfilling.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+from datetime import datetime
+
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+
+from catmaid.control.common import get_request_bool, urljoin
+from catmaid.control.authentication import requires_user_role
+from catmaid.models import Message, User, UserRole
+
+from celery.task import task
+
+from rest_framework.authtoken.models import Token
+
+
+# The path were server side exported files get stored in
+output_path = os.path.join(settings.MEDIA_ROOT, settings.MEDIA_EXPORT_SUBDIRECTORY)
+
+
+# @requires_user_role(UserRole.Annotate)
+def flood_fill_skeleton(response):
+    return JsonResponse({"success": True, "script":flood_fill()})
+
+
+@task()
+def flood_fill_async():
+    return None
+
+
+def flood_fill():
+    bash_script = """#!/usr/bin/env bash
+ssh {server_address}
+source {environment_path}
+cd {work_folder}
+{flood_fill_command}
+scp {output_files} {media_folder}""".format(
+        **{
+            "server_address": "server",
+            "environment_path": "environment",
+            "work_folder": "work_path",
+            "flood_fill_command": "python -m diluvian fill",
+            "output_files": "output",
+            "media_folder": "media",
+        }
+    )
+    return bash_script
